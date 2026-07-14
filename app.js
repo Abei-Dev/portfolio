9ac7fd9954858b13d35bf7141623d46355f95cf3
Emmanuel Duncan <ab.abeikuduncan@gmail.com>
2026-07-13 19:57:27 -1200

    Initial Project Portfolio

---
+++ app.js
@@ -0,0 +1,34 @@
+const sections = document.querySelectorAll("section")
+const navLinks = document.querySelectorAll(".nav-links a")
+
+window.addEventListener("scroll", updateActiveLink)
+
+updateActiveLink()
+
+function updateActiveLink() {
+    let currentSection = ""
+
+    const pageBottom =
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight - 10
+
+    if (pageBottom) {
+        currentSection = "contact"
+    } else {
+        sections.forEach(function(section) {
+            const sectionTop = section.offsetTop - 150
+
+            if (window.scrollY >= sectionTop) {
+                currentSection = section.id
+            }
+        })
+    }
+
+    navLinks.forEach(function(link) {
+        link.classList.remove("active")
+
+        if (link.getAttribute("href") === "#" + currentSection) {
+            link.classList.add("active")
+        }
+    })
+}
